70b6840e13dbb7ad498d384331e8e583e0efcb06
messiahsb <messiahs@buffalo.edu>
2026-01-08 21:49:44 -0500

    added a few more notes

--- tictactoe.py
+++ tictactoe.py
@@ -3,9 +3,19 @@
 #draw board
 
 #define game loop
+#   while game is not over continue
+
+#   select a square to make your move
+#   redraw board
+#   check if winning move
+#   else
+#   change turn to next player
+
+#   if winning move end game, congratulate winnder
+
 
 #define how turns will be played
 
 #define how to calculate if a win or a draw has been played
 
-#congratulate the winner
+
