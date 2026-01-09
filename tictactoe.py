e91892c3aa2c0244eb7e8ce88b847cbe7b8f6e63
messiahsb <messiahs@buffalo.edu>
2026-01-08 21:45:34 -0500

    defined a basic layout for what I want to go into the code

--- tictactoe.py
+++ tictactoe.py
@@ -1,34 +1,11 @@
-board = new_board()
+#initialize empty board
 
-# Loop through turns until the game is over
-loop forever:
-  # TODO: hmm I'm not sure how best to do this
-  # right now. No problem, I'll come back later.
-  current_player = ???
+#draw board
 
-  # Print the current state of the board
-  render(board)
+#define game loop
 
-  # Get the move that the current player is going
-  # to make.
-  move_co_ords = get_move()
+#define how turns will be played
 
-  # Make the move that we calculated above
-  make_move(board, move_co_ords, current_player)
+#define how to calculate if a win or a draw has been played
 
-  # Work out if there's a winner
-  winner = get_winner(board)
-
-  # If there is a winner, crown them the champion
-  # and exit the loop.
-  if winner is not None:
-    print "WINNER IS %s!!" % winner
-    break
-
-  # If there is no winner and the board is full,
-  # exit the loop.
-  if is_board_full(board):
-    print "IT'S A DRAW!!"
-    break
-
-  # Repeat until the game is over
+#congratulate the winner
